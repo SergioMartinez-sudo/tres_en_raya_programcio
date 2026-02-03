cfe89f06a3c86fce8c5b2444c7d95117eecdb163
SergioMartinez-sudo <sergiomartinezbarrero2008@gmail.com>
2026-02-03 09:49:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// We select the elements from the DOM
+
 const cells = document.querySelectorAll ('.cell');
 const statusDisplay = document.querySelectorAll('h2');
 let gameActive = true;
@@ -6,7 +6,7 @@ let currentPlayer = "x";
 let gameState = ["", "", "", "", "", "", "", "", ""]
 
 
-//Posibility WIN
+
 const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -17,3 +17,63 @@ const winningConditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
+
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = Array.from(cells).indexOf(clickedCell);
+
+
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerText = currentPlayer;
+    clickedCell.classList.add(currentPlayer.toLowerCase());
+}
+ 
+
+
+function handleResultValidation() {
+    let roundWon = false;
+
+    for (let i = 0; i < winningConditions.length; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }}
+
+
+
+        if (roundWon) {
+        statusDisplay.innerText = `¡El jugador ${currentPlayer} ha ganado!`;
+        gameActive = false;
+        return;
+    }
+
+    // Comprobar si hay empate
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerText = "¡Empate!";
+        gameActive = false;
+        return;
+    }

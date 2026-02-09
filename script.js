77d5c33355218e6c7dcbfed850d08788df872101
SergioMartinez-sudo <sergiomartinezbarrero2008@gmail.com>
2026-02-09 19:29:45 +0100

    ENTREGA FINAL

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// We select the elements from the DOM
-const cells = document.querySelectorAll ('.cell');
-const statusDisplay = document.querySelectorAll('h2');
+const cells = document.querySelectorAll('.cell');
+const statusDisplay = document.querySelector('h2');
 let gameActive = true;
-let currentPlayer = "x";
-let gameState = ["", "", "", "", "", "", "", "", ""]
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
 
-
-//Posibility WIN
 const winningConditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -18,41 +15,24 @@ const winningConditions = [
     [2, 4, 6]
 ];
 
-
-
-
-
-
-
-
-// Function to handle clicking on a cell
 function handleCellClick(clickedCellEvent) {
     const clickedCell = clickedCellEvent.target;
     const clickedCellIndex = Array.from(cells).indexOf(clickedCell);
 
-
-    // If the cell is already occupied or the game is over, we do nothing.
     if (gameState[clickedCellIndex] !== "" || !gameActive) {
         return;
     }
 
-
-    // We updated the status and interface
     handleCellPlayed(clickedCell, clickedCellIndex);
     handleResultValidation();
 }
 
-
-// Function to mark the cell
 function handleCellPlayed(clickedCell, clickedCellIndex) {
     gameState[clickedCellIndex] = currentPlayer;
     clickedCell.innerText = currentPlayer;
     clickedCell.classList.add(currentPlayer.toLowerCase());
 }
 
-
-
-// 5. Función para validar el resultado
 function handleResultValidation() {
     let roundWon = false;
 
@@ -77,27 +57,19 @@ function handleResultValidation() {
         return;
     }
 
-    // Comprobar si hay empate
     let roundDraw = !gameState.includes("");
     if (roundDraw) {
         statusDisplay.innerText = "¡Empate!";
         gameActive = false;
         return;
     }
-
-    // Si no han ganado ni empatado, cambiamos de jugador
     handlePlayerChange();
 }
 
-// 6. Función para cambiar de turno
 function handlePlayerChange() {
     currentPlayer = currentPlayer === "X" ? "O" : "X";
     statusDisplay.innerText = `Turno de ${currentPlayer}`;
 }
 
-// 7. Event listeners para las celdas
 cells.forEach(cell => cell.addEventListener('click', handleCellClick));
-
-// Inicializar el mensaje de turno
 statusDisplay.innerText = `Turno de ${currentPlayer}`;
-
